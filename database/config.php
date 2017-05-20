c0de1aab439ad583b1fe75dd33e35ee0a22059c9
Fajar Noormansyah <fnoormansyah@gmail.com>
2017-05-20 08:20:07 +0700

    Commit Pertama, Versi 1

---
+++ database/config.php
@@ -0,0 +1,9 @@
+<?php
+class config{
+public static  $LOGIN_USER='aempat1_login_user';
+public static $LOGIN_DETAIL='aempat1_login_detail';
+public static $MENUS='aempat1_menus';
+public static $ISADMIN='aempat1_isadmin';
+public static $MASTER_GROUP_DETAIL_LIST='aempat1_master_group_detail_list';
+}
+?>
